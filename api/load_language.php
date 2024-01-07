fc65efee7a7af2ad93b4704192ff76ef1b128850
speedjin1995 <speedjin1995@gmail.com>
2024-01-08 00:43:15 +0800

    added API

---
+++ api/load_language.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'db_connect.php';
+
+$lang = $db->query("SELECT * FROM message_resource");
+
+$languages = array();
+
+while ($row = mysqli_fetch_assoc($lang)) {
+    $code = $row['message_key_code'];
+
+    $languages['en'][$code] = $row['en'];
+    $languages['ch'][$code] = $row['ch'];
+    $languages['my'][$code] = $row['my'];
+    $languages['np'][$code] = $row['np'];
+}
+
+$db->close();
+
+echo json_encode(
+    array(
+        "status" => "success", 
+        "languages" => $languages
+    )
+);
+?>
