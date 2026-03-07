93f778dab50290afe58672087076b764817416d1
Ng Jih Bin <sky.ng@dcap.my>
2026-03-07 22:07:36 +0800

    excel added remark

--- php/exportExcel.php
+++ php/exportExcel.php
@@ -110,7 +110,7 @@ if(isset($_GET['id']) &&$_GET['id'] != null && $_GET['id'] != ''){
         // ================================
         // ✅ SECTION 3 — WEIGHT DATA
         // ================================
-        $excelData .= "WEIGHT_NO\tDATE_TIME\tGROSS\tCRATE\tNET\tUNIT\tNO_OF_CRATES\tBIRD_PER_CAGE\tBIRDS\tGENDER\tGRADE\tHOUSE_NO\tGROUP_NO\tAVE_WEIGHT\n";
+        $excelData .= "WEIGHT_NO\tDATE_TIME\tGROSS\tCRATE\tNET\tUNIT\tNO_OF_CRATES\tBIRD_PER_CAGE\tBIRDS\tGENDER\tGRADE\tHOUSE_NO\tGROUP_NO\tAVE_WEIGHT\tREMARK\n";
     
         $timestampData = json_decode($row['weight_time'], true);
         $weightData = json_decode($row['weight_data'], true);
@@ -132,7 +132,8 @@ if(isset($_GET['id']) &&$_GET['id'] != null && $_GET['id'] != ''){
                     $w['grade'],
                     $w['houseNumber'],
                     $w['groupNumber'],
-                    floatval($w['numberOfBirds']) > 0 ? number_format(floatval($w['netWeight'])/floatval($w['numberOfBirds']), 2) : '0.00'
+                    floatval($w['numberOfBirds']) > 0 ? number_format(floatval($w['netWeight'])/floatval($w['numberOfBirds']), 2) : '0.00',
+                    $w['remark'] ?? ''
                 ];
     
                 array_walk($weightRow, 'filterData');
