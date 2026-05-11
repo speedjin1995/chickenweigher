9e8494174ebdc69c5b04f533f856174820be6fc8
speedjin1995 <speedjin1995@gmail.com>
2026-05-11 15:25:35 +0800

    deployed template

---
+++ SQL/deploy.sql
@@ -0,0 +1,8 @@
+INSERT INTO `message_resource` (`message_key_code`, `en`, `zh`, `my`, `ne`) VALUES
+('actions_code', 'Actions', '操作', 'Tindakan', 'செயல்கள்'),
+('download_template_code', 'Download Template', '下载模板', 'Muat Turun Templat', 'டெம்ப்ளேட் பதிவிறக்கம்'),
+('error_log_code', 'Error Log', '错误日志', 'Log Ralat', 'பிழை பதிவு'),
+('upload_excel_file_code', 'Upload Excel File', '上传Excel文件', 'Muat Naik Fail Excel', 'எக்செல் கோப்பை பதிவேற்றவும்'),
+('preview_data_code', 'Preview Data', '预览数据', 'Pratonton Data', 'தரவை முன்னோட்டம்'),
+('number_code', 'No.', '编号', 'No.', 'எண்.')
+;
